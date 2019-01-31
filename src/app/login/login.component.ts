cc8eb61140bdb0d6b45237bf3f1e4f1683a865fd
BANU <stefan.banu@cgi.com>
2019-01-31 16:28:04 +0200

    added the username on the welcome page after the login success

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,20 +8,24 @@ import {Component, OnInit} from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  username = 'test'
+  username = 'stefan'
   password = ''
   errorMessage = 'Invalid Credentials'
   invalidLogin = false
 
-  constructor() {
-  }
+  //Router
+  // Angular.give me router
+  // dependency injection
+
+  constructor(private router: Router) {}
 
   ngOnInit() {
   }
 
   handleLogin() {
     // console.log(this.username);
-    if (this.username === 'test' && this.password === 'dummy') {
+    if (this.username === 'stefan' && this.password === 'dummy') {
+      this.router.navigate(['welcome', this.username])
       this.invalidLogin = false;
     } else {
       this.invalidLogin = true;
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+  message = 'some welcome message'
+  name = ''
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+
+    this.name = this.route.snapshot.params['name'];
   }
 
 }
