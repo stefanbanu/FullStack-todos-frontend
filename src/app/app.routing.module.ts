8ce4c6378b964c0f972daa2365d1eecc8f2d6f49
Stefan Banu <stefanbanu1@gmail.com>
2019-02-12 04:40:21 +0200

    customized the footer and changed the routes from href to router-link

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -10,6 +10,7 @@ const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'welcome/:name', component: WelcomeComponent},
   {path: 'todos', component: ListTodosComponent},
+
   {path: '**', component: ErrorComponent}
 ];
 
