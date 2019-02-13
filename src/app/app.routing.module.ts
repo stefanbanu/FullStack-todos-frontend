b04fd96bcefa2d0dd96594d0727f5b60c73d29b9
Stefan Banu <stefanbanu1@gmail.com>
2019-02-13 07:12:00 +0200

    added route-guard service, canActivate authentication

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -5,13 +5,14 @@ import {WelcomeComponent} from './welcome/welcome.component';
 import {ErrorComponent} from './error/error.component';
 import {ListTodosComponent} from './list-todos/list-todos.component';
 import {LogoutComponent} from './logout/logout.component';
+import {RouteGuardService} from './service/route-guard.service';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'welcome/:name', component: WelcomeComponent},
-  {path: 'todos', component: ListTodosComponent},
-  {path: 'logout', component: LogoutComponent},
+  {path: 'welcome/:name', component: WelcomeComponent, canActivate:[RouteGuardService]},
+  {path: 'todos', component: ListTodosComponent, canActivate:[RouteGuardService]},
+  {path: 'logout', component: LogoutComponent, canActivate:[RouteGuardService]},
 
   {path: '**', component: ErrorComponent}
 ];
