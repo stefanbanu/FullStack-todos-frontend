7f97b500978bc3b42d0ac450205c3531c1f2fadc
stefanbanu <stefan.banu@cgi.com>
2019-05-09 14:18:10 +0300

    added jpa and h2 in memory.

--- src/app/service/data/todo-data.service.ts
+++ src/app/service/data/todo-data.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Todo} from '../../list-todos/list-todos.component';
-import {API_URL} from '../../app.constants';
+import {API_URL, API_URL_JPA} from '../../app.constants';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +11,7 @@ export class TodoDataService {
   constructor(private httpClient: HttpClient) { }
 
   retrieveAllTodos(username) {
-    return this.httpClient.get<Todo[]>(`${API_URL}/users/${username}/todos`);
+    return this.httpClient.get<Todo[]>(`${API_URL_JPA}/users/${username}/todos`);
   }
 
   deleteTodo(username, id) {
@@ -19,7 +19,7 @@ export class TodoDataService {
   }
 
   retrieveTodo(username, id) {
-    return this.httpClient.get<Todo>(`${API_URL}/users/${username}/todos/${id}`);
+    return this.httpClient.get<Todo>(`${API_URL_JPA}/users/${username}/todos/${id}`);
   }
 
   updateTodo(username, id, todo) {
